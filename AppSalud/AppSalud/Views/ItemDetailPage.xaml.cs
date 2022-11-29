fbf1c1377dd12f0d1fe8cce42fa7268e408fbe8c
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-11-28 19:45:45 -0600

    Commit Inicial

---
+++ AppSalud/AppSalud/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AppSalud.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AppSalud.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
