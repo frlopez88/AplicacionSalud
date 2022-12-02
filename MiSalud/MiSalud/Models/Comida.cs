9fc2e9dcf1f49866f7916ca01123e6f3279e880c
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-12-01 20:15:11 -0600

    Commit de Comidas

--- MiSalud/MiSalud/Models/Comida.cs
+++ MiSalud/MiSalud/Models/Comida.cs
@@ -13,5 +13,49 @@ namespace MiSalud.Models
         public double calorias { get; set; }
         public DateTime fecha { get; set; }
 
+        public string toString() {
+
+            return fecha.ToString("MM-dd-yyyy") + " - " + nombre_plato + " - calorias: " + calorias + "\n\t";
+        }
+
+        public void CalculoCalorias() {
+
+
+            switch (nombre_plato) {
+
+                case "Pizza":
+                    calorias = 700;
+                    break;
+
+                case "Hamburgesa":
+                    calorias = 800;
+                    break;
+
+                case "Ensalada":
+                    calorias = 400;
+                    break;
+
+                case "Desayuno Tipico":
+                    calorias= 500;
+                    break;
+                case "Licuado":
+                    calorias = 200;
+                    break;
+                case "Mandarina":
+                    calorias = 80;
+                    break;
+                case "Banano":
+                    calorias = 110;
+                    break;
+                default:
+                    break;
+            
+            
+            }
+
+        }
+
+
+
     }
 }
---
+++ MiSalud/MiSalud/ViewModels/ViewModelComida.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+using MiSalud.Models;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace MiSalud.ViewModels
+{
+    public class ViewModelComida : INotifyPropertyChanged
+    {
+
+        public ViewModelComida() {
+
+            AbrirArchivo();
+
+
+            CrearComida = new Command( ()=> {
+
+                Comida c1 = new Comida()
+                {
+                    nombre_plato = this.plato, 
+                    fecha = this.fechaComida
+
+                };
+
+                c1.CalculoCalorias();
+                p.lista_comidas.Add(c1);
+
+                BinaryFormatter formatter = new BinaryFormatter();
+                string ruta = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal),
+                    "info.aut");
+                Stream archivo = new FileStream(ruta, FileMode.Create, FileAccess.Write, FileShare.None);
+                formatter.Serialize(archivo, p);
+                archivo.Close();
+
+                Resultado = "";
+
+                foreach (Comida c in p.lista_comidas) {
+
+                    Resultado += c.toString();
+
+                }
+
+
+            } );
+
+        
+        }
+
+        private void AbrirArchivo()
+        {
+
+            // Es una estructura 
+
+            try
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                string ruta = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal),
+                    "info.aut");
+                Stream archivo = new FileStream(ruta, FileMode.Open, FileAccess.Read, FileShare.None);
+
+                p = (Persona)formatter.Deserialize(archivo);
+                archivo.Close();
+
+                Resultado = "";
+
+                foreach (Comida x in p.lista_comidas)
+                {
+
+                    Resultado += x.toString();
+
+                }
+
+
+            }
+            catch (Exception e)
+            {
+
+
+            }
+
+
+
+        }
+
+
+        string resultado;
+
+        public string Resultado
+        {
+
+            get => resultado;
+            set
+            {
+
+                resultado = value;
+                var args = new PropertyChangedEventArgs(nameof(Resultado));
+                PropertyChanged?.Invoke(this, args);
+
+            }
+
+        }
+
+        Persona p = new Persona();
+
+        string plato;
+
+        public string Plato {
+
+            get => plato;
+            set {
+
+                plato = value;
+                var args = new PropertyChangedEventArgs(nameof(Plato));
+                PropertyChanged?.Invoke(this, args);
+
+            }
+
+        }
+
+
+        DateTime fechaComida = DateTime.Today;
+
+        public DateTime FechaComida
+        {
+
+            get => fechaComida;
+            set
+            {
+
+                fechaComida = value;
+                var args = new PropertyChangedEventArgs(nameof(FechaComida));
+                PropertyChanged?.Invoke(this, args);
+
+            }
+
+        }
+
+        DateTime fechaMin = DateTime.Today;
+
+        public DateTime FechaMin
+        {
+            get => fechaMin;
+            set
+            {
+
+                fechaMin = value;
+                var arg = new PropertyChangedEventArgs(nameof(FechaMin));
+                PropertyChanged?.Invoke(this, arg);
+
+            }
+        }
+
+        public Command CrearComida { get; }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
